951c462a435770f4fd8f0a3d97a39dce4692c84b
aripadrian <aripadrian01@gmail.com>
2022-01-10 21:21:34 +0700

    first init

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const multer = require('multer');
+require('dotenv').config()
+
+const AWS = require('aws-sdk');
+const app = express();
+const bodyParser = require('body-parser');
+const upload = multer();
+const s3 = new AWS.S3({
+    endpoint: process.env.ENDPOINT,
+    credentials: {
+        accessKeyId: process.env.ACCESS_KEY,
+        secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    },
+});
+
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    }),
+);
+
+
+app.post('/upload', upload.none(), (req, res) => {
+    buf = Buffer.from(
+        req.body.imageBinary.replace(/^data:image\/\w+;base64,/, ''),
+        'base64',
+    );
+
+    const params = {
+        Bucket: 'invitamu', // pass your bucket name
+        Key: `${Math.floor(Date.now() / 1000)}.png`,
+        Body: buf,
+        ACL: 'public-read',
+        ContentEncoding: 'base64',
+        ContentType: 'image/png', // file will be saved as testBucket/contacts.csv
+    };
+    s3.upload(params, (s3Err, data) => {
+        if (s3Err) {
+            res.json({
+                success: false,
+                error: s3Err,
+            });
+        } else {
+            res.json({
+                success: true,
+                data: data.Location,
+            });
+        }
+    });
+});
+
+app.listen(3000);
